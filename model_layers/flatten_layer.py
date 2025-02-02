2e791e7ff2fd7ec7867b063ca404522995afcf7a
SarvagnyaKunal <sarvagnya.3456@gmail.com>
2025-02-03 03:12:57 +0530

    removed eveerything for cleaning

--- model_layers/flatten_layer.py
+++
@@ -1,8 +0,0 @@
-class FlattenLayer:
-    
-    def forward(self, inputs):
-        self.inputs = inputs
-        self.outputs = self.inputs.reshape((self.inputs.shape[0], self.inputs.shape[1]*self.inputs.shape[2]*self.inputs.shape[3]))
-    
-    def backward(self, dvalues):
-        self.dinputs = dvalues.reshape((self.inputs.shape[0], self.inputs.shape[1], self.inputs.shape[2], self.inputs.shape[3]))
--- model_layers/relu_layer.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class ReLULayer:
-    def forward(self, input_array):
-        self.input = input_array
-        return np.maximum(0, input_array)
-    
-    def backward(self, d_output):
-        return d_output * (self.input > 0)
